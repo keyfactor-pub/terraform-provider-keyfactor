b52f8bf59cf49063cc4becd41322e89f72fe0dba
Hayden <49427552+m8rmclaren@users.noreply.github.com>
2021-10-29 21:54:49 -0400

    Rework schema retrieval, add metadata operations, add revoke failsafe

--- keyfactor/resource_keyfactor_certificate.go
+++ keyfactor/resource_keyfactor_certificate.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"keyfactor-go-client/pkg/keyfactor"
+	"log"
 	"strconv"
 )
 
@@ -21,6 +22,12 @@ func resourceCertificate() *schema.Resource {
 				Required: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
+						"revoke_on_destroy": &schema.Schema{
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Computed:    true,
+							Description: "Set to true to revoke certificate upon calling terraform destroy",
+						},
 						"csr": &schema.Schema{
 							Type:        schema.TypeString,
 							Optional:    true,
@@ -141,8 +148,23 @@ func resourceCertificate() *schema.Resource {
 						"metadata": &schema.Schema{
 							Type:        schema.TypeList,
 							Optional:    true,
-							Description: "",
-							Elem:        &schema.Schema{Type: schema.TypeString},
+							Description: "Metadata key-value pairs to be attached to certificate",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": &schema.Schema{
+										Type:        schema.TypeString,
+										Optional:    true,
+										Description: "Name of metadata field as seen in Keyfactor",
+										Elem:        &schema.Schema{Type: schema.TypeString},
+									},
+									"value": &schema.Schema{
+										Type:        schema.TypeString,
+										Optional:    true,
+										Description: "Metadata value",
+										Elem:        &schema.Schema{Type: schema.TypeString},
+									},
+								},
+							},
 						},
 						"certificate_format": &schema.Schema{
 							Type:        schema.TypeString,
@@ -155,9 +177,10 @@ func resourceCertificate() *schema.Resource {
 							Optional:    true,
 							Description: "Collection identifier used to validate user permissions (if service account has global permissions, this is not needed)",
 						},
-						"certificate": &schema.Schema{
-							Type:     schema.TypeString,
+						"certificates": &schema.Schema{
+							Type:     schema.TypeList,
 							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
 						},
 						"serial_number": &schema.Schema{
 							Type:     schema.TypeString,
@@ -196,6 +219,12 @@ func resourceCertificateCreate(ctx context.Context, d *schema.ResourceData, m in
 		i := certificate.(map[string]interface{})
 		subject := i["subject"].([]interface{})[0].(map[string]interface{}) // Extract subject data from schema
 		sans := i["sans"].([]interface{})[0].(map[string]interface{})       // Extract SANs from schema
+		metadata := i["metadata"].([]interface{})
+
+		format := i["certificate_format"].(string)
+		if format == "PEM" || format == "DER" || format == "P7B" {
+			format = "STORE" // Enroll endpoint expects STORE, PFX, or Zip
+		}
 
 		if i["csr"] != "" {
 			CSRArgs := &keyfactor.EnrollCSRFctArgs{
@@ -205,39 +234,30 @@ func resourceCertificateCreate(ctx context.Context, d *schema.ResourceData, m in
 				IncludeChain:         i["include_chain"].(bool),
 				CertFormat:           i["certificate_format"].(string),
 				CertificateSANs:      getSans(sans),
+				CertificateMetadata:  unpackMetadata(metadata),
 			}
 			response, err := keyfactor.EnrollCSR(CSRArgs, kfClientData)
 			if err != nil {
 				return diag.FromErr(err)
 			}
 
+			// Attempt to retrieve the correct certificate format
+			format = i["certificate_format"].(string)
+			if format == "PEM" || format == "DER" || format == "P7B" {
+				// Download certificate updates the certificates field of the enroll response
+				// with the correct certificate format
+				if err, response.Certificates = downloadCertificate(response.CertificateInformation.KeyfactorID, kfClientData, format); err != nil {
+					return diag.FromErr(err)
+				}
+			}
+
 			// Set computed schema
-			err = d.Set("serial_number", response.CertificateInformation.SerialNumber)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("issuer_dn", response.CertificateInformation.IssuerDN)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("thumbprint", response.CertificateInformation.Thumbprint)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("keyfactor_id", response.CertificateInformation.KeyfactorID)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("keyfactor_request_id", response.CertificateInformation.KeyfactorRequestID)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("certificate", response.Certificates)
-			if err != nil {
+			data := flattenEnrollResponse(response)
+			if err := d.Set("certificate", data); err != nil {
 				return diag.FromErr(err)
 			}
 
-			// Set resource ID to tell Terraform that operation was successful
+			// Set resource ID to tell Terraform that operation was successful*
 			d.SetId(strconv.Itoa(response.CertificateInformation.KeyfactorID))
 		} else {
 
@@ -249,8 +269,9 @@ func resourceCertificateCreate(ctx context.Context, d *schema.ResourceData, m in
 				Template:                    i["cert_template"].(string),
 				IncludeChain:                i["include_chain"].(bool),
 				RenewalCertificateId:        i["renewal_certificate_id"].(int),
-				CertFormat:                  i["certificate_format"].(string),
+				CertFormat:                  format,
 				CertificateSANs:             getSans(sans), // if no SANs are specified, this field is nil
+				CertificateMetadata:         unpackMetadata(metadata),
 				CertificateSubject: keyfactor.CertificateSubject{
 					SubjectCommonName:         subject["subject_common_name"].(string),
 					SubjectLocality:           subject["subject_locality"].(string),
@@ -266,29 +287,19 @@ func resourceCertificateCreate(ctx context.Context, d *schema.ResourceData, m in
 				return diag.FromErr(err)
 			}
 
+			// Attempt to retrieve the correct certificate format
+			format = i["certificate_format"].(string)
+			if format == "PEM" || format == "DER" || format == "P7B" {
+				// Download certificate updates the certificates field of the enroll response
+				// with the correct certificate format
+				if err, response.Certificates = downloadCertificate(response.CertificateInformation.KeyfactorID, kfClientData, format); err != nil {
+					return diag.FromErr(err)
+				}
+			}
+
 			// Set computed schema
-			err = d.Set("serial_number", response.CertificateInformation.SerialNumber)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("issuer_dn", response.CertificateInformation.IssuerDN)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("thumbprint", response.CertificateInformation.Thumbprint)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("keyfactor_id", response.CertificateInformation.KeyfactorID)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("keyfactor_request_id", response.CertificateInformation.KeyfactorRequestID)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-			err = d.Set("certificate", response.Certificate)
-			if err != nil {
+			data := flattenEnrollResponse(response)
+			if err := d.Set("certificate", data); err != nil {
 				return diag.FromErr(err)
 			}
 
@@ -299,10 +310,49 @@ func resourceCertificateCreate(ctx context.Context, d *schema.ResourceData, m in
 	return diags
 }
 
+func flattenEnrollResponse(response *keyfactor.EnrollResponse) []interface{} {
+	if response != nil {
+		temp := make([]interface{}, 1, 1)
+		data := make(map[string]interface{})
+
+		// Build an interface array and populate with certificates inside EnrollResponse
+		certs := make([]interface{}, len(response.Certificates), len(response.Certificates))
+		for i, cert := range response.Certificates {
+			certs[i] = cert
+		}
+		// Assign response data to associated schema
+		data["certificates"] = certs
+		data["serial_number"] = response.CertificateInformation.SerialNumber
+		data["issuer_dn"] = response.CertificateInformation.IssuerDN
+		data["thumbprint"] = response.CertificateInformation.Thumbprint
+		data["keyfactor_id"] = response.CertificateInformation.KeyfactorID
+		data["keyfactor_request_id"] = response.CertificateInformation.KeyfactorRequestID
+		temp[0] = data // For now, only one certificate can be enrolled per resource instance
+		return temp
+	}
+	return make([]interface{}, 0)
+}
+
+func downloadCertificate(id int, api *keyfactor.APIClient, format string) (error, []string) {
+	downloadArgs := &keyfactor.DownloadCertArgs{
+		CertID:       id,
+		IncludeChain: true,
+		CertFormat:   format,
+	}
+
+	resp, err := keyfactor.DownloadCertificate(downloadArgs, api)
+	if err != nil {
+		return err, nil
+	}
+	temp := []string{resp.Content}
+	return nil, temp
+}
+
 func getSans(s interface{}) *keyfactor.SANs {
 	sans := &keyfactor.SANs{}
 
 	// Retrieve individual SANs for each category and append to new SANs data structure
+	// Maybe separate these for loops to their own function?
 	for _, san := range s.(map[string]interface{})["san_ip4"].([]interface{}) {
 		sans.IP4 = append(sans.IP4, san.(string))
 	}
@@ -319,11 +369,16 @@ func getSans(s interface{}) *keyfactor.SANs {
 	return sans
 }
 
-func parseMetadata(d *schema.ResourceData) *keyfactor.Metadata {
-	// Metadata specified in .tf files using TypeMap
-	// Default state representation of maps include the count of items inside a map (%)
-	// To change the expected behavior according to a specific Keyfactor instance, adjust expected values.
-
+func unpackMetadata(m []interface{}) []keyfactor.StringTuple {
+	// Unpack metadata expects []interface{} containing a list of lists of key-value pairs
+	if len(m) > 0 {
+		temp := make([]keyfactor.StringTuple, len(m), len(m)) // size of m is the number of metadata fields provided by .tf file
+		for i, field := range m {
+			temp[i].Elem1 = field.(map[string]interface{})["name"].(string)  // Unless changed in the future, this interface
+			temp[i].Elem2 = field.(map[string]interface{})["value"].(string) // will always have 'name' and 'value'
+		}
+		return temp
+	}
 	return nil
 }
 
@@ -339,25 +394,49 @@ func resourceCertificateUpdate(ctx context.Context, d *schema.ResourceData, m in
 	return diags
 }
 
+func updateMetadata(id int, api *keyfactor.APIClient, metadata []keyfactor.StringTuple) error {
+	args := &keyfactor.UpdateMetadataArgs{
+		CertID:              id,
+		CertificateMetadata: metadata,
+	}
+
+	err := keyfactor.UpdateMetadata(args, api)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func resourceCertificateDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 
+	log.Println("[INFO] Deleting certificate resource")
+
 	// When Terraform Destroy is called, we want Keyfactor to revoke the certificate.
 	// Typically, this is not common in production
-
-	revokeArgs := &keyfactor.RevokeCertArgs{
-		CertificateIds: []int{d.Get("keyfactor_id").(int)}, // Certificate ID expects array of integers
-		Reason:         5,                                  // reason = 5 means Cessation of Operation
-		Comment:        "Terraform resource delete called on provider with associated cert ID",
-		CollectionId:   d.Get("collection_id").(int),
-	}
-
 	kfClientData := m.(*keyfactor.APIClient)
 
-	err := keyfactor.RevokeCert(revokeArgs, kfClientData)
-	if err != nil {
-		return diag.FromErr(err)
-	}
+	certificates := d.Get("certificate").([]interface{})
 
+	for _, certificate := range certificates {
+		i := certificate.(map[string]interface{})
+		// Only revoke if revoke_on_destroy is true
+		if i["revoke_on_destroy"].(bool) == true {
+			log.Println("[INFO] Revoking certificate in Keyfactor")
+			revokeArgs := &keyfactor.RevokeCertArgs{
+				CertificateIds: []int{i["keyfactor_id"].(int)}, // Certificate ID expects array of integers
+				Reason:         5,                              // reason = 5 means Cessation of Operation
+				Comment:        "Terraform destroy called on provider with associated cert ID",
+				CollectionId:   i["collection_id"].(int),
+			}
+
+			err := keyfactor.RevokeCert(revokeArgs, kfClientData)
+			if err != nil {
+				return diag.FromErr(err)
+			}
+		}
+
+	}
 	return diags
 }
--- keyfactor/resource_keyfactor_store.go
+++ keyfactor/resource_keyfactor_store.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"keyfactor-go-client/pkg/keyfactor"
+	"log"
 )
 
 func resourceStore() *schema.Resource {
@@ -13,17 +15,204 @@ func resourceStore() *schema.Resource {
 		UpdateContext: resourceStoreUpdate,
 		DeleteContext: resourceStoreDelete,
 		Schema: map[string]*schema.Schema{
-			"container_id": &schema.Schema{
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Description: "Container identifier of the store's associated certificate store container",
+			"store": &schema.Schema{
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"container_id": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Container identifier of the store's associated certificate store container.",
+						},
+						"client_machine": &schema.Schema{
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "Client machine name; value depends on certificate store type. See API reference guide",
+						},
+						"store_path": &schema.Schema{
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "Path to the new certificate store on a target. Format varies depending on type.",
+						},
+						"cert_store_inventory_job_id": &schema.Schema{
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "GUID identifying the inventory job for the certificate store. Null if inventory is not configured",
+						},
+						"cert_store_type": &schema.Schema{
+							Type:        schema.TypeInt,
+							Required:    true,
+							Description: "Integer specifying the store type. Specific types require different parameters.",
+						},
+						"approved": &schema.Schema{
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "Bool that indicates the approval status of store created. Default is true, omit if unsure",
+						},
+						"create_if_missing": &schema.Schema{
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "Bool that indicates if the store should be created with information provided. Valid only for JKS type, omit if unsure",
+						},
+						"properties_json": &schema.Schema{
+							Type:        schema.TypeList,
+							Required:    true,
+							Description: "JSON key-value pair strings representing the properties for the specified certificate store",
+							Elem:        &schema.Schema{Type: schema.TypeString},
+						},
+						"agent_id": &schema.Schema{
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "String indicating the Keyfactor Command GUID of the orchestrator for the created store",
+						},
+						"agent_assigned": &schema.Schema{
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "Bool indicating if there is an orchestrator assigned to the new certificate store",
+						},
+						"container_name": &schema.Schema{
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Name of certificate store's associated container, if applicable",
+						},
+						"inventory_schedule": &schema.Schema{
+							Type:        schema.TypeList,
+							Optional:    true,
+							Description: "Inventory schedule for new certificate store",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"immediate": &schema.Schema{
+										Type:        schema.TypeBool,
+										Optional:    true,
+										Description: "Boolean that indicates whether the job should run immediately",
+									},
+									"interval": &schema.Schema{
+										Type:        schema.TypeList,
+										Optional:    true,
+										Description: "Indicates that the job should be scheduled to run every x minutes",
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"minutes": &schema.Schema{
+													Type:        schema.TypeInt,
+													Required:    true,
+													Description: "An integer indicating the number of minutes between each interval",
+												},
+											},
+										},
+									},
+									"daily": &schema.Schema{
+										Type:        schema.TypeList,
+										Optional:    true,
+										Description: "Indicates that the job should be scheduled to run every day at the same time",
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"time": &schema.Schema{
+													Type:        schema.TypeString,
+													Required:    true,
+													Description: "The date and time to next run the job. The date and time should be given using the ISO 8601 UTC time format",
+												},
+											},
+										},
+									},
+									"exactly_once": &schema.Schema{
+										Type:        schema.TypeList,
+										Optional:    true,
+										Description: "Indicates that the job should be scheduled to run at the time specified",
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"time": &schema.Schema{
+													Type:        schema.TypeString,
+													Required:    true,
+													Description: "The date and time to next run the job. The date and time should be given using the ISO 8601 UTC time format",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						"reenrollment_status": &schema.Schema{
+							Type:        schema.TypeList,
+							Optional:    true,
+							Description: "Configures the re-enrollnent function with accompanying data",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"data": &schema.Schema{
+										Type:        schema.TypeBool,
+										Optional:    true,
+										Description: "Indicates whether the certificate store can use the re-enrollment function",
+									},
+									"agent_id": &schema.Schema{
+										Type:        schema.TypeString,
+										Optional:    true,
+										Description: "String indicating the Keyfactor Command GUID of the orchestrator that can re-enroll the store",
+									},
+									"message": &schema.Schema{
+										Type:        schema.TypeString,
+										Optional:    true,
+										Description: "Reason the certificate store cannot reenroll, if applicable",
+									},
+									"job_properties": &schema.Schema{
+										Type:        schema.TypeList,
+										Optional:    true,
+										Description: "List of key-value pairs as strings for the unique parameters defined for store type",
+										Elem:        &schema.Schema{Type: schema.TypeString},
+									},
+									"custom_alias_allowed": &schema.Schema{
+										Type:        schema.TypeInt,
+										Optional:    true,
+										Description: "An integer indicating the option for a custom alias for this certificate store",
+									},
+								},
+							},
+						},
+						"set_new_password_allowed": &schema.Schema{
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "Indicates whether the store password can be changed",
+						},
+						"password": &schema.Schema{
+							Type:        schema.TypeList,
+							Optional:    true,
+							Description: "Configures credential options for certificate store",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"value": &schema.Schema{
+										Type:        schema.TypeString,
+										Optional:    true,
+										Description: "Configures a password to be stored a Keyfactor secret",
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 	}
 }
 
 func resourceStoreCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	return nil
+	var diags diag.Diagnostics
+
+	stores := d.Get("store").([]interface{})
+
+	for _, store := range stores {
+		i := store.(map[string]interface{})
+		newStoreArgs := &keyfactor.CreateStoreFctArgs{
+			ContainerId:     i["container_id"].(int),
+			ClientMachine:   i["client_machine"].(string),
+			StorePath:       i["store_path"].(string),
+			CertStoreType:   i["cert_store_type"].(int),
+			Approved:        i["approved"].(bool),
+			CreateIfMissing: i["create_if_missing"].(bool),
+		}
+		log.Printf("args: %d", newStoreArgs.ContainerId)
+	}
+	// CertStoreInventoryJobId: i["cert_store_inventory_job_id"].(string),
+	return diags
 }
 
 func resourceStoreRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {

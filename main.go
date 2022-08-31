ec011601143e0b5d4e9d49deb5bc5e436020a45c
sbailey <1661003+spbsoluble@users.noreply.github.com>
2022-08-30 20:20:03 -0700

    fix(ci): Update docs gen

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 // Run the docs generation tool, check its repository for more information on how it works and how docs
 // can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+//go:generate go run tfplugindocs
 
 // To run these tools, run 'go generate'
 

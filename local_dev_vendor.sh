30bfce89a74eb1ce02362afeadc4fc6b5afdf694
sbailey <1661003+spbsoluble@users.noreply.github.com>
2023-10-12 08:51:52 -0700

    chore(docs): Update CHANGELOG.md

--- local_dev_vendor.sh
+++ local_dev_vendor.sh
@@ -3,4 +3,7 @@ make vendor
 cd vendor/github.com/Keyfactor
 rm -rf keyfactor-go-client
 ln -s "$HOME/GolandProjects/keyfactor-go-client" .
+cd ../spbsoluble
+rm -rf go-pkcs12
+ln -s "$HOME/GolandProjects/go-pkcs12" .
 cd ../../..

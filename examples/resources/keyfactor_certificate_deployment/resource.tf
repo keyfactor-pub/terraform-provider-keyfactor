a91342369f7fadfc4989fd059cea8bc148caf586
sbailey <1661003+spbsoluble@users.noreply.github.com>
2023-10-24 14:33:25 -0700

    chore(deps): Bump `keyfactor-go-client` version

--- examples/resources/keyfactor_certificate_deployment/resource.tf
+++ examples/resources/keyfactor_certificate_deployment/resource.tf
@@ -19,12 +19,12 @@ data "keyfactor_certificate" "ca_cert" {
 
 # Deploy the CA certificate to the certificate store
 resource "keyfactor_certificate_deployment" "ca_cert_deployment" {
-  certificate_id = data.keyfactor_certificate.ca_cert.certificate_id
+  certificate_id       = data.keyfactor_certificate.ca_cert.certificate_id
   # The Keyfactor Command internal certificate ID
   certificate_store_id = data.keyfactor_certificate_store.my_cert_store.id # The Keyfactor Command certificate store ID
   certificate_alias    = data.keyfactor_certificate.ca_cert.thumbprint
   # Alias to use for the certificate in the store
-  job_parameters = {
+  job_parameters       = {
     #Optional entry parameters to provide to the deployment job. These will only be used if the orchestrator extension supports them.
     "Region"    = "us-east-1"
     "Account"   = "1234567890"

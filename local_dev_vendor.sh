ff118710e6a319cb321700ca21c616ac9b795aa4
sbailey <1661003+spbsoluble@users.noreply.github.com>
2024-01-30 13:06:18 -0800

    chore: Delete unrelated dev script

--- local_dev_vendor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-make vendor
-cd vendor/github.com/Keyfactor
-rm -rf keyfactor-go-client
-ln -s "$HOME/GolandProjects/keyfactor-go-client" .
-cd ../spbsoluble
-rm -rf go-pkcs12
-ln -s "$HOME/GolandProjects/go-pkcs12" .
-cd ../../..

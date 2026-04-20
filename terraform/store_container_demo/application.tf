40bdf1150291f6d479f72b82271bbb551ada343e
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2026-04-20 11:02:40 -0700

    chore(demo): remove store_container_demo from git tracking (to be added in separate PR)

--- terraform/store_container_demo/application.tf
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------------------------
-# Application (container) that both stores will be linked to.
-# Validates that container_name and application_name both resolve to the same
-# Command application/container concept.
-# ---------------------------------------------------------------------------
-resource "keyfactor_application" "demo" {
-  name                = "Store Container Demo${var.suffix}"
-  schedule_daily_time = "2026-01-01T12:00:00Z"
-}

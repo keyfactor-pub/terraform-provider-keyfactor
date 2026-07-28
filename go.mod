keyfactor-pub/terraform-provider-keyfactor

commit 6db030658dcad68332c99c4feb2ad99ba55b001a

chore(docs): Update CHANGELOG.md

go.mod
@@ -5,7 +5,7 @@ go 1.24.0
 require (
 	github.com/Keyfactor/keyfactor-auth-client-go v1.5.0
 	github.com/Keyfactor/keyfactor-go-client-sdk/v24 v24.1.1
-	github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6-rc.1
+	github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6
 	github.com/hashicorp/terraform-plugin-framework v0.10.0
 	github.com/hashicorp/terraform-plugin-go v0.18.0
 	github.com/hashicorp/terraform-plugin-log v0.10.0

go.sum
@@ -20,8 +20,8 @@ github.com/Keyfactor/keyfactor-auth-client-go v1.5.0 h1:sq7SGkJeTtDspFSuX2oJxTmF
 github.com/Keyfactor/keyfactor-auth-client-go v1.5.0/go.mod h1:rFBZPMSHWwWuUwE1kXhLsDaOxjGiHMbXTTEni8Dmufo=
 github.com/Keyfactor/keyfactor-go-client-sdk/v24 v24.1.1 h1:snWSopug5HD/GH7EByOYUs/dBU+b4mLpgBpDbHVdGJU=
 github.com/Keyfactor/keyfactor-go-client-sdk/v24 v24.1.1/go.mod h1:xIbpgJ9eYfcYeSM0VzP5Q3ifgLCf3yGKKyZtWmqqOi8=
-github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6-rc.1 h1:LuD3yxK8Qalv8aI+cbRomwAr70QWatpSBJb6XPVUvDk=
-github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6-rc.1/go.mod h1:9NCImFBYq78JBzIX6evxvVlLX5oaGyMonpkKQGcOiZk=
+github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6 h1:U3TXgIx55zHL2FmNrRLZJKP8LAn7LHklM8MzHSbCl7A=
+github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6/go.mod h1:9NCImFBYq78JBzIX6evxvVlLX5oaGyMonpkKQGcOiZk=
 github.com/Microsoft/go-winio v0.6.2 h1:F2VQgta7ecxGYO8k3ZZz3RS8fVIXVxONVUPlNERoyfY=
 github.com/Microsoft/go-winio v0.6.2/go.mod h1:yd8OoFMLzJbo9gZq8j5qaps8bJ9aShtEA8Ipt1oGCvU=
 github.com/ProtonMail/go-crypto v1.1.6 h1:ZcV+Ropw6Qn0AX9brlQLAUXfqLBc7Bl+f/DmNxpLfdw=

commit 638f6fafe659a8c225181dcd0ab3517fee5cffed

fix(deps): re-pin keyfactor-go-client/v3 to v3.5.6-rc.1
The certificate-store-type resource/data-source are written against the
rc.1 API where CertificateStoreType.{LocalStore,ServerRequired,PowerShell,
BlueprintAllowed} are plain bool. The final v3.5.6 release changed these to
*bool, so building against v3.5.6 fails to compile (CI Build + goreleaser).

Commit 6db0306 (a docs commit) inadvertently reverted the pin from
v3.5.6-rc.1 to v3.5.6; this restores it. go mod tidy keeps rc.1.

go.mod
@@ -5,7 +5,7 @@ go 1.24.0
 require (
 	github.com/Keyfactor/keyfactor-auth-client-go v1.5.0
 	github.com/Keyfactor/keyfactor-go-client-sdk/v24 v24.1.1
-	github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6
+	github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6-rc.1
 	github.com/hashicorp/terraform-plugin-framework v0.10.0
 	github.com/hashicorp/terraform-plugin-go v0.18.0
 	github.com/hashicorp/terraform-plugin-log v0.10.0

go.sum
@@ -20,8 +20,8 @@ github.com/Keyfactor/keyfactor-auth-client-go v1.5.0 h1:sq7SGkJeTtDspFSuX2oJxTmF
 github.com/Keyfactor/keyfactor-auth-client-go v1.5.0/go.mod h1:rFBZPMSHWwWuUwE1kXhLsDaOxjGiHMbXTTEni8Dmufo=
 github.com/Keyfactor/keyfactor-go-client-sdk/v24 v24.1.1 h1:snWSopug5HD/GH7EByOYUs/dBU+b4mLpgBpDbHVdGJU=
 github.com/Keyfactor/keyfactor-go-client-sdk/v24 v24.1.1/go.mod h1:xIbpgJ9eYfcYeSM0VzP5Q3ifgLCf3yGKKyZtWmqqOi8=
-github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6 h1:U3TXgIx55zHL2FmNrRLZJKP8LAn7LHklM8MzHSbCl7A=
-github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6/go.mod h1:9NCImFBYq78JBzIX6evxvVlLX5oaGyMonpkKQGcOiZk=
+github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6-rc.1 h1:LuD3yxK8Qalv8aI+cbRomwAr70QWatpSBJb6XPVUvDk=
+github.com/Keyfactor/keyfactor-go-client/v3 v3.5.6-rc.1/go.mod h1:9NCImFBYq78JBzIX6evxvVlLX5oaGyMonpkKQGcOiZk=
 github.com/Microsoft/go-winio v0.6.2 h1:F2VQgta7ecxGYO8k3ZZz3RS8fVIXVxONVUPlNERoyfY=
 github.com/Microsoft/go-winio v0.6.2/go.mod h1:yd8OoFMLzJbo9gZq8j5qaps8bJ9aShtEA8Ipt1oGCvU=
 github.com/ProtonMail/go-crypto v1.1.6 h1:ZcV+Ropw6Qn0AX9brlQLAUXfqLBc7Bl+f/DmNxpLfdw=